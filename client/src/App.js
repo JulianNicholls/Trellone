e38d4d2d09caf8bfb44d0bfc1b75d8d6c0c89785
Julian Nicholls <juliannicholls29@gmail.com>
2018-10-02 22:36:58 +0100

    Added client via c-r-a

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container has-background-primary">
+          <h1 className="has-text-white is-size-1 has-text-centered">Trellone</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
