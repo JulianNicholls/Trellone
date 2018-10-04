25e7fb2d1c7213bb4a6517a2c62091d0e95b5e6d
Julian Nicholls <juliannicholls29@gmail.com>
2018-10-04 17:38:20 +0100

    Add archived field to Card model.

--- models/Card.js
+++ models/Card.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cardSchema = new mongoose.Schema({
   text: { type: String, required: true },
   order: Number,
+  archived: { type: Boolean, default: false },
   ownerId: mongoose.Schema.Types.ObjectId,
   boardId: mongoose.Schema.Types.ObjectId,
   listId: mongoose.Schema.Types.ObjectId
