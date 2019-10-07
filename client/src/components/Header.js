0528f9f64ef1899f7c33e46bbd32b366d7a86c62
Julian Nicholls <juliannicholls29@gmail.com>
2019-10-07 21:24:10 +0100

    Add header

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div className="brand">Trellone</div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
