e931932094514e43555b9cb943ed7bc622b564e0
Julian Nicholls <juliannicholls29@gmail.com>
2019-10-09 16:13:18 +0100

    Add login, signup, and logout to UserContext

--- client/src/context/user.js
+++ client/src/context/user.js
@@ -1,17 +1,86 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 const UserContext = createContext();
 
-export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    token: '',
-    displayName: '',
-    avatarURL: '',
-    email: '',
-  });
+const EMPTY_USER = {
+  displayName: '',
+  avatarURL: '',
+  email: '',
+};
 
-  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(EMPTY_USER);
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const loadUser = async token => {
+      try {
+        const response = await axios.get('/api/users/current', {
+          headers: {
+            Authorization: token,
+          },
+        });
+
+        console.log('UPlU:', response);
+        setToken(token);
+        setUser(response.data.user);
+      } catch (err) {
+        console.log('Token expired');
+      }
+    };
+
+    const token = localStorage.trelloneAuth;
+
+    if (token) loadUser(token);
+  }, []);
+
+  const login = async (email, password) => {
+    try {
+      const response = await axios.post('/auth/login', { email, password });
+
+      console.log(response);
+
+      const { token, user } = response.data;
+      setToken(token);
+      localStorage.trelloneAuth = token;
+      setUser(user);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const signup = async (email, password) => {
+    try {
+      const response = await axios.post('/auth/signup', { email, password });
+
+      const { token, user } = response.data;
+      setToken(token);
+      localStorage.trelloneAuth = token;
+      setUser(user);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem('trelloneAuth');
+    setToken(null);
+    setUser(EMPTY_USER);
+  };
+
+  const userState = {
+    token,
+    user,
+    login,
+    signup,
+    logout,
+  };
+
+  return (
+    <UserContext.Provider value={userState}>{children}</UserContext.Provider>
+  );
 };
 
 UserProvider.propTypes = {
