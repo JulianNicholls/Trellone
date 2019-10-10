2e58ae418d2d2524a31216c4ad41b1c57dc88142
Julian Nicholls <juliannicholls29@gmail.com>
2019-10-10 17:09:26 +0100

    Update Board Card to show background URL.
    Card now has a link to the lists page.

--- client/src/pages/BoardsPage.js
+++ client/src/pages/BoardsPage.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { useBoards } from '../context/board';
 
 const BoardsPage = () => {
-  const boards = useBoards();
+  const { boards } = useBoards();
 
   const renderBoards = () =>
     boards.map(({ _id, name, backgroundURL }) => (
       <div key={_id} className="board-card">
-        <div className="board-card__header">{name}</div>
-        <p>
-          {_id}
-          {backgroundURL}
-        </p>
+        <Link to={`/lists/${_id}`}>
+          <div className="board-card__content">
+            {backgroundURL && <img src={backgroundURL} alt="Board background" />}
+          </div>
+          <div className="board-card__footer">{name}</div>
+        </Link>
       </div>
     ));
 
@@ -20,7 +22,12 @@ const BoardsPage = () => {
     <main>
       <h1 className="centred">Boards</h1>
 
-      <div className="board-grid">{renderBoards()}</div>
+      <div className="board-grid">
+        {renderBoards()}
+        <div className="board-card">
+          <button>Create New</button>
+        </div>
+      </div>
     </main>
   );
 };
