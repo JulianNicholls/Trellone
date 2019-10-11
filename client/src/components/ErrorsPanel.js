ccf9e511adf0ebca1fd97007039d7e4a10fd35d9
Julian Nicholls <juliannicholls29@gmail.com>
2019-10-11 20:44:51 +0100

    Re-factor login / signup errors panel

---
+++ client/src/components/ErrorsPanel.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ErrorsPanel = ({ errors }) => {
+  if (errors.length === 0) return null;
+
+  return (
+    <div className="errors">
+      {errors.map(e => (
+        <div key={e}>{e}</div>
+      ))}
+    </div>
+  );
+};
+
+ErrorsPanel.propTypes = {
+  errors: PropTypes.array.isRequired,
+};
+
+export default ErrorsPanel;
